620437305a6a07afe0f60f15c208609c84f2d77c
jaehyun <h15790@naver.com>
2019-11-04 16:02:54 +0900

    first commit

---
+++ HashSample.java
@@ -0,0 +1,19 @@
+package y;
+
+import java.util.HashSet;
+
+public class HashSample {
+
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<>();
+		set.add("A");
+		set.add("B");
+		set.add("C");
+		set.add("D");
+		set.add("E");
+		set.add(new String("A"));
+		set.remove("A");
+		System.out.println(set);
+	}
+
+}
